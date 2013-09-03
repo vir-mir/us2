26f6551c64be129a74815ee14c01aa339bc52c86
vir-mir <info@vir-mir.ru>
2013-09-03 20:18:19 +1100

    бд

--- _db/us20130903.sql
+++ _db/us20130903.sql
@@ -1,6 +1,6 @@
 ﻿# SQL Manager 2007 for MySQL 4.5.0.4
 # ---------------------------------------
-# Host     : localhost
+# Host     : komtender.local
 # Port     : 3306
 # Database : us
 
@@ -46,7 +46,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `app_label` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `auth_permission` table : 
@@ -63,7 +63,7 @@ CREATE TABLE `auth_permission` (
   UNIQUE KEY `content_type_id` (`content_type_id`,`codename`),
   KEY `auth_permission_37ef4eb4` (`content_type_id`),
   CONSTRAINT `content_type_id_refs_id_d043b34a` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `auth_group_permissions` table : 
@@ -103,7 +103,7 @@ CREATE TABLE `auth_user` (
   `date_joined` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `auth_user_groups` table : 
@@ -215,6 +215,83 @@ CREATE TABLE `menu_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
+# Structure for the `site_user_duties` table : 
+#
+
+DROP TABLE IF EXISTS `site_user_duties`;
+
+CREATE TABLE `site_user_duties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date DEFAULT NULL,
+  `lft` int(10) unsigned NOT NULL,
+  `rght` int(10) unsigned NOT NULL,
+  `tree_id` int(10) unsigned NOT NULL,
+  `level` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `site_user_duties_410d0aac` (`parent_id`),
+  KEY `site_user_duties_329f6fb3` (`lft`),
+  KEY `site_user_duties_e763210f` (`rght`),
+  KEY `site_user_duties_ba470c4a` (`tree_id`),
+  KEY `site_user_duties_20e079f4` (`level`),
+  CONSTRAINT `parent_id_refs_id_70cdaa9e` FOREIGN KEY (`parent_id`) REFERENCES `site_user_duties` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `site_user_staff` table : 
+#
+
+DROP TABLE IF EXISTS `site_user_staff`;
+
+CREATE TABLE `site_user_staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `patronymic` varchar(255) DEFAULT NULL,
+  `date_expire` date DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `site_user_dateduties` table : 
+#
+
+DROP TABLE IF EXISTS `site_user_dateduties`;
+
+CREATE TABLE `site_user_dateduties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `date_expire` date DEFAULT NULL,
+  `staff_id` int(11) NOT NULL,
+  `duty_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `site_user_dateduties_f0a7d083` (`staff_id`),
+  KEY `site_user_dateduties_7185ea2d` (`duty_id`),
+  CONSTRAINT `duty_id_refs_id_94f00652` FOREIGN KEY (`duty_id`) REFERENCES `site_user_duties` (`id`),
+  CONSTRAINT `staff_id_refs_id_c455a768` FOREIGN KEY (`staff_id`) REFERENCES `site_user_staff` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `site_user_staff_user` table : 
+#
+
+DROP TABLE IF EXISTS `site_user_staff_user`;
+
+CREATE TABLE `site_user_staff_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `staff_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `site_user_staff_user_staff_id_78484c28_uniq` (`staff_id`,`user_id`),
+  KEY `site_user_staff_user_f0a7d083` (`staff_id`),
+  KEY `site_user_staff_user_6340c63c` (`user_id`),
+  CONSTRAINT `user_id_refs_id_1894af3d` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
+  CONSTRAINT `staff_id_refs_id_aa73abb7` FOREIGN KEY (`staff_id`) REFERENCES `site_user_staff` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+#
 # Structure for the `south_migrationhistory` table : 
 #
 
@@ -226,81 +303,7 @@ CREATE TABLE `south_migrationhistory` (
   `migration` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `user_staff` table : 
-#
-
-DROP TABLE IF EXISTS `user_staff`;
-
-CREATE TABLE `user_staff` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `patronymic` varchar(255) DEFAULT NULL,
-  `date_expire` date DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `user_dateduties` table : 
-#
-
-DROP TABLE IF EXISTS `user_dateduties`;
-
-CREATE TABLE `user_dateduties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
-  `date_expire` date DEFAULT NULL,
-  `staff_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_dateduties_f0a7d083` (`staff_id`),
-  CONSTRAINT `staff_id_refs_id_409329f6` FOREIGN KEY (`staff_id`) REFERENCES `user_staff` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `user_duties` table : 
-#
-
-DROP TABLE IF EXISTS `user_duties`;
-
-CREATE TABLE `user_duties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `date` date DEFAULT NULL,
-  `lft` int(10) unsigned NOT NULL,
-  `rght` int(10) unsigned NOT NULL,
-  `tree_id` int(10) unsigned NOT NULL,
-  `level` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_duties_410d0aac` (`parent_id`),
-  KEY `user_duties_329f6fb3` (`lft`),
-  KEY `user_duties_e763210f` (`rght`),
-  KEY `user_duties_ba470c4a` (`tree_id`),
-  KEY `user_duties_20e079f4` (`level`),
-  CONSTRAINT `parent_id_refs_id_9c386f5a` FOREIGN KEY (`parent_id`) REFERENCES `user_duties` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-#
-# Structure for the `user_staff_user` table : 
-#
-
-DROP TABLE IF EXISTS `user_staff_user`;
-
-CREATE TABLE `user_staff_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `staff_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_staff_user_staff_id_629e9851_uniq` (`staff_id`,`user_id`),
-  KEY `user_staff_user_f0a7d083` (`staff_id`),
-  KEY `user_staff_user_6340c63c` (`user_id`),
-  CONSTRAINT `user_id_refs_id_eb1a7047` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
-  CONSTRAINT `staff_id_refs_id_a5728b8e` FOREIGN KEY (`staff_id`) REFERENCES `user_staff` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 #
 # Data for the `auth_group` table  (LIMIT 0,500)
@@ -327,7 +330,10 @@ INSERT INTO `django_content_type` (`id`, `name`, `app_label`, `model`) VALUES
   (9,'item','menu','item'),
   (11,'duties','user','duties'),
   (12,'staff','user','staff'),
-  (13,'date duties','user','dateduties');
+  (13,'date duties','user','dateduties'),
+  (14,'duties','site_user','duties'),
+  (15,'staff','site_user','staff'),
+  (16,'date duties','site_user','dateduties');
 COMMIT;
 
 #
@@ -370,7 +376,16 @@ INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALU
   (36,'Can delete staff',12,'delete_staff'),
   (37,'Can add date duties',13,'add_dateduties'),
   (38,'Can change date duties',13,'change_dateduties'),
-  (39,'Can delete date duties',13,'delete_dateduties');
+  (39,'Can delete date duties',13,'delete_dateduties'),
+  (40,'Can add duties',14,'add_duties'),
+  (41,'Can change duties',14,'change_duties'),
+  (42,'Can delete duties',14,'delete_duties'),
+  (43,'Can add staff',15,'add_staff'),
+  (44,'Can change staff',15,'change_staff'),
+  (45,'Can delete staff',15,'delete_staff'),
+  (46,'Can add date duties',16,'add_dateduties'),
+  (47,'Can change date duties',16,'change_dateduties'),
+  (48,'Can delete date duties',16,'delete_dateduties');
 COMMIT;
 
 #
@@ -412,7 +427,9 @@ COMMIT;
 #
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES 
-  (1,'pbkdf2_sha256$10000$2Hy6BvdY9hOc$kS5MeM5MSmN2ZMtYQoapZHRqKFmGwzOexgmCoSVHnWM=','2013-09-02 07:04:48',1,'admin','','','info@vir-mir.ru',1,1,'2013-08-30 05:44:30');
+  (1,'pbkdf2_sha256$10000$2Hy6BvdY9hOc$kS5MeM5MSmN2ZMtYQoapZHRqKFmGwzOexgmCoSVHnWM=','2013-09-02 23:10:06',1,'admin','','','info@vir-mir.ru',1,1,'2013-08-30 05:44:30'),
+  (2,'pbkdf2_sha256$10000$mZ05uqs009aG$XHwsEG9+LkJx530RhVLiVFL5uzUYKjD0hONsjpuGicY=','2013-09-03 06:58:14',0,'test','','','',0,1,'2013-09-03 06:47:07'),
+  (3,'pbkdf2_sha256$10000$6jYRsYE7sGYC$GjskVYnLndGRfNhM0R+MLxvP9742FYK2TfVs+KtzRLY=','2013-09-03 06:57:31',0,'new','','','',0,1,'2013-09-03 06:57:31');
 COMMIT;
 
 #
@@ -431,7 +448,7 @@ COMMIT;
 INSERT INTO `django_admin_log` (`id`, `action_time`, `user_id`, `content_type_id`, `object_id`, `object_repr`, `action_flag`, `change_message`) VALUES 
   (1,'2013-09-01 23:07:42',1,2,'1','Ad',1,''),
   (2,'2013-09-01 23:07:57',1,2,'2','Administration',1,''),
-  (3,'2013-09-01 23:08:18',1,3,'1','admin',2,'Изменен password и groups.');
+  (3,'2013-09-01 23:08:18',1,3,'1','admin',2,'');
 COMMIT;
 
 #
@@ -439,7 +456,9 @@ COMMIT;
 #
 
 INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES 
+  ('30887kazq3zpm1anfcfv477dr4cpvwst','NTI4ZDE2ZjliOGM5YzhmMDdlMDhhMzY2ZGUyMjc2ZDY1ZGM1N2Y4MjqAAn1xAShVEl9hdXRoX3VzZXJfYmFja2VuZHECVSlkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZHEDVQ1fYXV0aF91c2VyX2lkcQSKAQF1Lg==','2013-09-16 23:10:07'),
   ('8qqptzvowwktl1m1pd09aly75gnh637v','NTI4ZDE2ZjliOGM5YzhmMDdlMDhhMzY2ZGUyMjc2ZDY1ZGM1N2Y4MjqAAn1xAShVEl9hdXRoX3VzZXJfYmFja2VuZHECVSlkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZHEDVQ1fYXV0aF91c2VyX2lkcQSKAQF1Lg==','2013-09-15 22:10:28'),
+  ('dwfo6rh6f0fcoitrdspbc25b1ioy43ri','ZTRhNzUwNDBjNDFmYWZhYjJkODViMDk2OWE1YWNjN2IzYWU1ZTliOTqAAn1xAShVEl9hdXRoX3VzZXJfYmFja2VuZHECVSlkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZHEDVQ1fYXV0aF91c2VyX2lkcQSKAQJ1Lg==','2013-09-17 06:58:14'),
   ('forhkewheqos5g6ka237pnagnniucu2e','NTI4ZDE2ZjliOGM5YzhmMDdlMDhhMzY2ZGUyMjc2ZDY1ZGM1N2Y4MjqAAn1xAShVEl9hdXRoX3VzZXJfYmFja2VuZHECVSlkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZHEDVQ1fYXV0aF91c2VyX2lkcQSKAQF1Lg==','2013-09-16 07:04:48');
 COMMIT;
 
@@ -452,23 +471,71 @@ INSERT INTO `django_site` (`id`, `domain`, `name`) VALUES
 COMMIT;
 
 #
+# Data for the `site_user_duties` table  (LIMIT 0,500)
+#
+
+INSERT INTO `site_user_duties` (`id`, `parent_id`, `name`, `date`, `lft`, `rght`, `tree_id`, `level`) VALUES 
+  (1,NULL,'фыв','2013-09-03',1,40,1,0),
+  (2,1,'Под должность','2013-09-03',2,25,1,1),
+  (3,2,'еще под должность','2013-09-03',3,8,1,2),
+  (4,2,'Еще под должность','2013-09-03',9,14,1,2),
+  (5,1,'Под должность 1','2013-09-03',26,31,1,1),
+  (6,5,'под должность23','2013-09-03',27,30,1,2),
+  (7,2,'123','2013-09-03',15,20,1,2),
+  (8,2,'123-1','2013-09-03',21,24,1,2),
+  (9,8,'12','2013-09-03',22,23,1,3),
+  (10,7,'3455укацууафыафыв','2013-09-03',16,19,1,3),
+  (11,10,'йцуйцуйцуйц','2013-09-03',17,18,1,4),
+  (12,3,'лялял','2013-09-03',4,5,1,3),
+  (13,3,'тополя','2013-09-03',6,7,1,3),
+  (14,4,'не_яяя','2013-09-03',10,13,1,3),
+  (15,14,'ываываа','2013-09-03',11,12,1,4),
+  (16,6,'sdfsdf','2013-09-03',28,29,1,3),
+  (17,1,'Вротая верхняяфыв','2013-09-03',32,35,1,1),
+  (18,17,'123','2013-09-03',33,34,1,2),
+  (19,1,'ввв','2013-09-03',36,37,1,1),
+  (20,1,'фывва','2013-09-03',38,39,1,1);
+COMMIT;
+
+#
+# Data for the `site_user_staff` table  (LIMIT 0,500)
+#
+
+INSERT INTO `site_user_staff` (`id`, `name`, `surname`, `patronymic`, `date_expire`, `date`) VALUES 
+  (1,'','Вакансия','',NULL,'2013-09-03'),
+  (2,'Алексей','Фирсов','Андреевич',NULL,'2013-09-03'),
+  (3,'Тествов','Тест','Тестовичь',NULL,'2013-09-03');
+COMMIT;
+
+#
+# Data for the `site_user_dateduties` table  (LIMIT 0,500)
+#
+
+INSERT INTO `site_user_dateduties` (`id`, `date`, `date_expire`, `staff_id`, `duty_id`) VALUES 
+  (1,'2013-09-02','2013-09-03',3,5),
+  (2,'2013-09-03','2013-09-03',3,5),
+  (3,'2013-09-03',NULL,2,5);
+COMMIT;
+
+#
+# Data for the `site_user_staff_user` table  (LIMIT 0,500)
+#
+
+INSERT INTO `site_user_staff_user` (`id`, `staff_id`, `user_id`) VALUES 
+  (1,2,1),
+  (7,3,2),
+  (9,3,3);
+COMMIT;
+
+#
 # Data for the `south_migrationhistory` table  (LIMIT 0,500)
 #
 
 INSERT INTO `south_migrationhistory` (`id`, `app_name`, `migration`, `applied`) VALUES 
   (1,'menu','0001_initial','2013-09-02 05:33:22'),
   (2,'mptt','0001_initial','2013-09-02 05:34:06'),
-  (3,'user','0001_initial','2013-09-02 06:28:04');
-COMMIT;
-
-#
-# Data for the `user_duties` table  (LIMIT 0,500)
-#
-
-INSERT INTO `user_duties` (`id`, `parent_id`, `name`, `date`, `lft`, `rght`, `tree_id`, `level`) VALUES 
-  (1,NULL,'werwer','2013-09-02',1,4,1,0),
-  (2,NULL,'1Под уровень','2013-09-02',1,2,2,0),
-  (3,1,'под wer','2013-09-02',2,3,1,1);
+  (3,'site_user','0001_initial','2013-09-02 22:56:25'),
+  (4,'site_user','0002_auto__add_field_dateduties_duty','2013-09-03 07:12:57');
 COMMIT;
 
 
